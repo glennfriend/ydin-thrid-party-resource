7c340f2350d14af496f28f669308b7001b96eba0
glenn <glenn.profile@gmail.com>
2019-12-03 10:01:42 +0800

    refactor(config)

--- config/third-party-resource.php
+++ config/third-party-resource.php
@@ -1,5 +1,21 @@
 <?php
 
+$local = [
+    'beefree_url'   => 'https://beefree.io',
+    'bi_url'        => 'http://127.0.0.1:8000',
+    'kidguard_url'  => 'https://staging.kidguard.com',
+    'kidguard_auth' => env('KIDGUARD_API_AUTHORIZATION'),
+];
+
+$staging = array_merge($local, [
+    'bi_url'        => 'https://staging.bi.kidguard.com',
+]);
+
+$production = array_merge($local, [
+    'bi_url'        => 'https://bi.kidguard.com',
+    'kidguard_url'  => 'https://app.kidguard.com',
+]);
+
 return [
 
     /**
@@ -11,24 +27,9 @@ return [
      * 由 'use' 決定使用哪一組 API
      */
     'api' => [
-        'local' => [
-            'beefree_url'       => 'https://beefree.io',
-            'bi_url'            => 'http://127.0.0.1:8000',
-            'kidguard_url'      => 'https://staging.kidguard.com',
-            'kidguard_auth'     => env('KIDGUARD_API_AUTHORIZATION'),
-        ],
-        'staging' => [
-            'beefree_url'       => 'https://beefree.io',
-            'bi_url'            => 'https://staging.bi.kidguard.com',
-            'kidguard_url'      => 'https://staging.kidguard.com',
-            'kidguard_auth'     => env('KIDGUARD_API_AUTHORIZATION'),
-        ],
-        'production' => [
-            'beefree_url'       => 'https://beefree.io',
-            'bi_url'            => 'https://bi.kidguard.com',
-            'kidguard_url'      => 'https://app.kidguard.com',
-            'kidguard_auth'     => env('KIDGUARD_API_AUTHORIZATION'),
-        ],
+        'local'      => $local,
+        'staging'    => $staging,
+        'production' => $production,
     ],
 
 ];
